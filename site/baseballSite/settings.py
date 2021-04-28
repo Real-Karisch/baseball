57f98cc1ab5a3b3c7144548e7077adb5a5b95694
Jack Karisch <jack.karisch@gmail.com>
2021-04-28 02:05:54 -0500

    Trying different database settings to see what works

--- site/baseballSite/settings.py
+++ site/baseballSite/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'baseballSite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-"""
+
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+#DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 """
 DATABASES = {
     'default': {
@@ -92,7 +92,7 @@ DATABASES = {
         'PORT': 5432
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
